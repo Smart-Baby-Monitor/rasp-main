67539785aa32709ab21f67a2bea9c1fb4aaaf9f8
Daaki Benjamin <richarddaaki4@gmail.com>
2023-06-27 09:29:54 +0300

    changed to relative folders

--- analyze.sh
+++ analyze.sh
@@ -1,1 +1,1 @@
-docker run --rm -v `pwd`:/opt/baby_cry_rpi -w /opt/baby_cry_rpi -it mikkl/rpi2-babycry:squashed bash cry_detection.sh
+docker run --rm -v `pwd`: . -w . -it mikkl/rpi2-babycry:squashed bash cry_detection.sh
