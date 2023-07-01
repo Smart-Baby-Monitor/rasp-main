1716f143825eaaf678a4f2a193ef627a9d211f6f
Daaki Benjamin <richarddaaki4@gmail.com>
2023-07-01 15:44:34 +0300

    added check for list type

--- analyze_data.py
+++ analyze_data.py
@@ -11,46 +11,48 @@ videos = db.selectQuery("SELECT * FROM videos WHERE label IS NULL LIMIT "+ str(2
 audios = db.selectQuery("SELECT * FROM audios WHERE label IS NULL LIMIT "+ str(20))
 motions = db.selectQuery("SELECT * FROM motions WHERE label IS NULL LIMIT "+str(20))
 logger.info("Using upto 3 threads to analyze data data")
-logger.info(f"Labeling audios {len(audios)}")
-model = Audio()
-for audio in audios :
-    try:
-        model.load_data(audio)
-        logger.info(f"Analysing audio {model.getId()}")
-        audio_label = label_audio(audio)
-        logger.info(f"audio labeled as {audio_label}")
-        db.update(model.table,{"label":audio_label},{"id":model.getId()})
-        logger.info("Audio Labelled successfully")
-    except Exception as e:
-        logger.error(e)
+if type(audios) == 'list':
+    logger.info(f"Labeling audios {len(audios)}")
+    model = Audio()
+    for audio in audios :
+        try:
+            model.load_data(audio)
+            logger.info(f"Analysing audio {model.getId()}")
+            audio_label = label_audio(audio)
+            logger.info(f"audio labeled as {audio_label}")
+            db.update(model.table,{"label":audio_label},{"id":model.getId()})
+            logger.info("Audio Labelled successfully")
+        except Exception as e:
+            logger.error(e)
 
 
-logger.info(f"Labeling motions {len(motions)}")
-model = Motion()
-for motion in motions :
-    try:
-        model.load_data(motion)
-        logger.info(f"Analysing motion {model.getId()}")
-        motion_label = label_motion(motion)
-        logger.info(f"motion labeled as {motion_label}")
-        db.update(model.table,{"label":motion_label},{"id":model.getId()})
-        logger.info("Motion Labelled successfully")
-    except Exception as e:
-        logger.error(e)
+if type(motions) == "list":
+    logger.info(f"Labeling motions {len(motions)}")
+    model = Motion()
+    for motion in motions :
+        try:
+            model.load_data(motion)
+            logger.info(f"Analysing motion {model.getId()}")
+            motion_label = label_motion(motion)
+            logger.info(f"motion labeled as {motion_label}")
+            db.update(model.table,{"label":motion_label},{"id":model.getId()})
+            logger.info("Motion Labelled successfully")
+        except Exception as e:
+            logger.error(e)
 
-
-logger.info(f"Labeling videos {len(videos)}")
-model = Video()
-for video in videos :
-    try:
-        model.load_data(video)
-        logger.info(f"Analysing video {model.getId()}")
-        video_label = label_video(motion)
-        logger.info(f"video labeled as {video_label}")
-        db.update(model.table,{"label":video_label},{"id":model.getId()})
-        logger.info("Video Labelled successfully")
-    except Exception as e:
-        logger.error(e)
+if type(videos) == "list":
+    logger.info(f"Labeling videos {len(videos)}")
+    model = Video()
+    for video in videos :
+        try:
+            model.load_data(video)
+            logger.info(f"Analysing video {model.getId()}")
+            video_label = label_video(motion)
+            logger.info(f"video labeled as {video_label}")
+            db.update(model.table,{"label":video_label},{"id":model.getId()})
+            logger.info("Video Labelled successfully")
+        except Exception as e:
+            logger.error(e)
 
 
 
