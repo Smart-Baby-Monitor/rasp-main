202bd72e741397d195d3b2bcb988021b629404a4
Daaki Benjamin <richarddaaki4@gmail.com>
2023-07-01 15:38:23 +0300

    commented out unused code

--- data_analysis/classifier.py
+++ data_analysis/classifier.py
@@ -20,67 +20,67 @@ def classify_audio(audio_file_path):
 
     # Initializations - Getting different paths 
         
-    load_path_model = os.path.normpath('{}/model/'.format(os.path.dirname(os.path.abspath(__file__))))
-    save_path = os.path.normpath('{}/prediction/'.format(os.path.dirname(os.path.abspath(__file__))))
+    # load_path_model = os.path.normpath('{}/model/'.format(os.path.dirname(os.path.abspath(__file__))))
+    # save_path = os.path.normpath('{}/prediction/'.format(os.path.dirname(os.path.abspath(__file__))))
+    pass
+    
+    # # Read audio signal
+    # file_reader = Reader(os.path.join(audio_file_path))
+    # play_list = file_reader.read_audio_file()
 
     
-    # Read audio signal
-    file_reader = Reader(os.path.join(audio_file_path))
-    play_list = file_reader.read_audio_file()
+    # # iterate on play_list for feature engineering and prediction
+    # # FEATURE ENGINEERING
+    # # Feature extraction
+    # engineer = FeatureEngineer()
 
-    
-    # iterate on play_list for feature engineering and prediction
-    # FEATURE ENGINEERING
-    # Feature extraction
-    engineer = FeatureEngineer()
+    # play_list_processed = list()
 
-    play_list_processed = list()
+    # for signal in play_list:
+    #     tmp = engineer.feature_engineer(signal)
+    #     play_list_processed.append(tmp)
 
-    for signal in play_list:
-        tmp = engineer.feature_engineer(signal)
-        play_list_processed.append(tmp)
+    # # MAKE PREDICTION
+    # # https://stackoverflow.com/questions/41146759/check-sklearn-version-before-loading-model-using-joblib
+    # with warnings.catch_warnings():
+    #   warnings.simplefilter("ignore", category=UserWarning)
 
-    # MAKE PREDICTION
-    # https://stackoverflow.com/questions/41146759/check-sklearn-version-before-loading-model-using-joblib
-    with warnings.catch_warnings():
-      warnings.simplefilter("ignore", category=UserWarning)
+    #   with open((os.path.join(load_path_model, 'model.pkl')), 'rb') as fp:
+    #       model = pickle.load(fp)
 
-      with open((os.path.join(load_path_model, 'model.pkl')), 'rb') as fp:
-          model = pickle.load(fp)
+    # predictor = BabyCryPredictor(model)
 
-    predictor = BabyCryPredictor(model)
+    # predictions = list()
+    # string_predictions = list()
+    # for signal in play_list_processed:
+    #     [string_prediction, is_baby_crying] = predictor.classify(signal)
+    #     predictions.append(is_baby_crying)
+    #     string_predictions.append(string_prediction)
 
-    predictions = list()
-    string_predictions = list()
-    for signal in play_list_processed:
-        [string_prediction, is_baby_crying] = predictor.classify(signal)
-        predictions.append(is_baby_crying)
-        string_predictions.append(string_prediction)
-
-    # MAJORITY VOTE Just tries to know if the baby is crying 
+    # # MAJORITY VOTE Just tries to know if the baby is crying 
    
-    majority_voter = MajorityVoter(predictions)
-    is_baby_crying = majority_voter.vote()
+    # majority_voter = MajorityVoter(predictions)
+    # is_baby_crying = majority_voter.vote()
 
-    # Save prediction result
-    with open(os.path.join(save_path, 'prediction.txt'), 'a+') as text_file:
-        text_file.write("{0}".format(is_baby_crying))
+    # # Save prediction result
+    # with open(os.path.join(save_path, 'prediction.txt'), 'a+') as text_file:
+    #     text_file.write("{0}".format(is_baby_crying))
     
     
-    return [string_predictions ,is_baby_crying]
+    # return [string_predictions ,is_baby_crying]
 
 def label_audio(audio_file_path):
     return "Silence"
-    [predictions, is_baby_crying] = classify_audio(audio_file_path)
-    categories = {"Crying":0,"Silence":0,"Noise":0,"Laughing":0}
-    for prediction in predictions:
-        categories[prediction] +=1
-    total = len(predictions) 
-    categories["Crying"] = categories['301 - Crying baby']/total*100
-    categories["Silence"] = categories['901 - Silence']/total*100
-    categories["Noise"] = categories['902 - Noise']/total*100
-    categories['Laughing'] = categories['903 - Baby laugh']/total*100
-    return get_max_category(categories)
+    # [predictions, is_baby_crying] = classify_audio(audio_file_path)
+    # categories = {"Crying":0,"Silence":0,"Noise":0,"Laughing":0}
+    # for prediction in predictions:
+    #     categories[prediction] +=1
+    # total = len(predictions) 
+    # categories["Crying"] = categories['301 - Crying baby']/total*100
+    # categories["Silence"] = categories['901 - Silence']/total*100
+    # categories["Noise"] = categories['902 - Noise']/total*100
+    # categories['Laughing'] = categories['903 - Baby laugh']/total*100
+    # return get_max_category(categories)
 
 def label_motion(motion_file):
     motion_data = load_json_file(motion_file)
