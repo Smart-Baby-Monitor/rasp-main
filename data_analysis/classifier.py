0561fca7160f854ead3751ac6f801e642e514fc9
Daaki Benjamin <richarddaaki4@gmail.com>
2023-07-11 10:25:27 +0300

    integrated model for vieos

--- data_analysis/classifier.py
+++ data_analysis/classifier.py
@@ -6,6 +6,11 @@ import os
 import pickle
 import sys
 import warnings
+from data_analysis import Reader
+import cv2
+import numpy as np
+from data_analysis.baby_cry_predictor import BabyCryPredictor
+from data_analysis.feature_engineer import FeatureEngineer
 
 # egg_path = '{}/../lib/baby_cry_detection-1.1-py2.7.egg'.format(os.path.dirname(os.path.abspath(__file__)))
 # sys.path.append(egg_path)
@@ -20,67 +25,57 @@ def classify_audio(audio_file_path):
 
     # Initializations - Getting different paths 
         
-    # load_path_model = os.path.normpath('{}/model/'.format(os.path.dirname(os.path.abspath(__file__))))
-    # save_path = os.path.normpath('{}/prediction/'.format(os.path.dirname(os.path.abspath(__file__))))
-    pass
+    load_path_model = os.path.normpath('{}/model/'.format(os.path.dirname(os.path.abspath(__file__))))
+    save_path = os.path.normpath('{}/prediction/'.format(os.path.dirname(os.path.abspath(__file__))))
     
-    # # Read audio signal
-    # file_reader = Reader(os.path.join(audio_file_path))
-    # play_list = file_reader.read_audio_file()
+    # Read audio signal
+    file_reader = Reader(os.path.join(audio_file_path))
+    play_list = file_reader.read_audio_file()
 
     
-    # # iterate on play_list for feature engineering and prediction
-    # # FEATURE ENGINEERING
-    # # Feature extraction
-    # engineer = FeatureEngineer()
+    # iterate on play_list for feature engineering and prediction
+    # FEATURE ENGINEERING
+    # Feature extraction
+    engineer = FeatureEngineer()
 
-    # play_list_processed = list()
+    play_list_processed = list()
 
-    # for signal in play_list:
-    #     tmp = engineer.feature_engineer(signal)
-    #     play_list_processed.append(tmp)
+    for signal in play_list:
+        tmp = engineer.feature_engineer(signal)
+        play_list_processed.append(tmp)
 
-    # # MAKE PREDICTION
-    # # https://stackoverflow.com/questions/41146759/check-sklearn-version-before-loading-model-using-joblib
-    # with warnings.catch_warnings():
-    #   warnings.simplefilter("ignore", category=UserWarning)
+    # MAKE PREDICTION
+    # https://stackoverflow.com/questions/41146759/check-sklearn-version-before-loading-model-using-joblib
+    with warnings.catch_warnings():
+      warnings.simplefilter("ignore", category=UserWarning)
 
-    #   with open((os.path.join(load_path_model, 'model.pkl')), 'rb') as fp:
-    #       model = pickle.load(fp)
+      with open((os.path.join(load_path_model, 'model.pkl')), 'rb') as fp:
+        model = pickle.load(fp)
 
-    # predictor = BabyCryPredictor(model)
+    predictor = BabyCryPredictor(model)
 
-    # predictions = list()
-    # string_predictions = list()
-    # for signal in play_list_processed:
-    #     [string_prediction, is_baby_crying] = predictor.classify(signal)
-    #     predictions.append(is_baby_crying)
-    #     string_predictions.append(string_prediction)
-
-    # # MAJORITY VOTE Just tries to know if the baby is crying 
-   
-    # majority_voter = MajorityVoter(predictions)
-    # is_baby_crying = majority_voter.vote()
-
-    # # Save prediction result
-    # with open(os.path.join(save_path, 'prediction.txt'), 'a+') as text_file:
-    #     text_file.write("{0}".format(is_baby_crying))
+    predictions = list()
+    string_predictions = list()
+    for signal in play_list_processed:
+        [string_prediction, is_baby_crying] = predictor.classify(signal)
+        predictions.append(is_baby_crying)
+        string_predictions.append(string_prediction)
     
     
-    # return [string_predictions ,is_baby_crying]
+    return [string_predictions ,is_baby_crying]
 
 def label_audio(audio_file_path):
-    return "Silence"
-    # [predictions, is_baby_crying] = classify_audio(audio_file_path)
-    # categories = {"Crying":0,"Silence":0,"Noise":0,"Laughing":0}
-    # for prediction in predictions:
-    #     categories[prediction] +=1
-    # total = len(predictions) 
-    # categories["Crying"] = categories['301 - Crying baby']/total*100
-    # categories["Silence"] = categories['901 - Silence']/total*100
-    # categories["Noise"] = categories['902 - Noise']/total*100
-    # categories['Laughing'] = categories['903 - Baby laugh']/total*100
-    # return get_max_category(categories)
+   
+    [predictions, is_baby_crying] = classify_audio(audio_file_path)
+    categories = {"Crying":0,"Silence":0,"Noise":0,"Laughing":0}
+    for prediction in predictions:
+        categories[prediction] +=1
+    total = len(predictions) 
+    categories["Crying"] = categories['301 - Crying baby']/total*100
+    categories["Silence"] = categories['901 - Silence']/total*100
+    categories["Noise"] = categories['902 - Noise']/total*100
+    categories['Laughing'] = categories['903 - Baby laugh']/total*100
+    return get_max_category(categories)
 
 def label_motion(motion_file):
     motion_data = load_json_file(motion_file)
@@ -96,10 +91,72 @@ def label_motion(motion_file):
     else:
         return "Motion"
     
-def label_video(audio_file_path):
-    
-    return "None" #get_max_category(categories)
+def label_video(video_file_path):
+    # Define the path to your video
+    video_path = video_file_path
+    # Load the pre-trained face detector model
+    model_file = "opencv_face_detector_uint8.pb"
+    config_file = "opencv_face_detector.pbtxt"
+    load_path_model = os.path.normpath('{}/model/'.format(os.path.dirname(os.path.abspath(__file__))))
+    load_path_config = load_path_model
+    model_path =  os.path.join(load_path_model, model_file)
+    config_path = os.path.join(load_path_config,config_file)
+    net = cv2.dnn.readNetFromTensorflow(model_path, config_path)
+    # Open the video capture
+    cap = cv2.VideoCapture(video_path)
 
+    # Initialize a counter to keep track of the number of faces
+    face_count = 0
+
+    # Set the parameters for face detection
+    confidence_threshold = 0.8
+    input_image_size = (300, 300)
+    scale_factor = 1.5
+
+    while cap.isOpened():
+        # Read the next frame from the video
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+    # Resize the frame if it's too large
+    frame = cv2.resize(frame, input_image_size)
+
+    # Create a blob from the frame and perform forward pass
+    blob = cv2.dnn.blobFromImage(frame, 1.0, input_image_size, [104, 117, 123], swapRB=True, crop=False)
+    net.setInput(blob)
+    detections = net.forward()
+
+    # Iterate over the detected faces
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        # Filter out weak detections
+        if confidence > confidence_threshold:
+            face_count += 1
+
+            # Get the coordinates of the bounding box
+            box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
+            (x, y, w, h) = box.astype(int)
+
+            # Draw the bounding box around the face
+            cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 2)
+
+            # Display the frame with the bounding boxes
+            cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+    # Print the total number of faces detected
+    if face_count :
+        return "Detected"
+    return "Not Detected"
 
 def get_max_category(categories):
     max_category = max(categories, key=categories.get)
